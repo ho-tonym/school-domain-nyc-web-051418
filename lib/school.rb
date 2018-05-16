59cd5ae3e8994f5a05fcd1bbdcea8b937e2702f5
Tony Ho <ho.tonym@gmail.com>
2018-05-16 04:08:32 +0000

    Done.

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,32 @@
-# code here!
+require "pry"
+
+class School
+
+  def initialize(school)
+    @school = school
+    @roster = {}
+  end
+
+
+  def roster
+    @roster
+  end
+
+  def add_student(student, number)
+    current = []
+    if @roster[number] != nil
+      current = @roster[number]
+    end
+    @roster[number] = current << student
+  end
+
+  def grade(number)
+    @roster[number]
+  end
+
+  def sort
+    @roster.each do |number, student_names|
+      student_names.sort!
+    end
+  end
+end
